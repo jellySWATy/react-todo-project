9019802cbd1ea5c9d705585e19a084a88f0dd17b
jellySWATy <111crafter111@gmail.com>
2020-01-24 09:14:04 +0300

    Added basic application structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <h1>ToDo list</h1>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>ToDo list</h1>
+      </div>
+    )
+  }
 }
-
-export default App;
